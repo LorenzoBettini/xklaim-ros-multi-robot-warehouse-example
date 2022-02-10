58c4b41a56855e03a28e8611cc34f8741b9edfcb
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-02-10 17:34:57 +0100

    created project

---
+++ xklaim-ros-robot-collective-warehouse-example/src-gen/coordination/Hello.java
@@ -0,0 +1,9 @@
+package coordination;
+
+@SuppressWarnings("all")
+public class Hello {
+  public static void main(final String[] args) throws Exception {
+    HelloNet helloNet = new HelloNet();
+    helloNet.addNodes();
+  }
+}
